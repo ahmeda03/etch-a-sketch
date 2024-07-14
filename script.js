ab310221478a211b827239af5d9fb5f1a1da2b87
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-13 20:02:11 -0400

    Alter sketchpad styles and dynamically re-size sketchpad

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-generateSketchPad();
-
 const sketchContainer = document.querySelector(".sketchContainer");
-sketchContainer.addEventListener('mouseover', activateBox);
+const resizeButton = document.querySelector(".promptButton");
+
+generateSketchPad();
+sketchContainer.addEventListener("mouseover", activateBox);
+resizeButton.addEventListener("click", resizeSketchPad);
 
 function activateBox(event) {
     let boxID = event.target.id;
@@ -11,7 +13,7 @@ function activateBox(event) {
     }
 }
 
-function generateSketchPad() {
+function generateSketchPad(numOfBoxes = 16) {
     /*
     INIT container by grabbing reference to sketch pad container
     INIT sketchpad width and sketchpad height to 16 (number of boxes)
@@ -21,11 +23,14 @@ function generateSketchPad() {
         Insert the into the container of the page
     END FOR
     */
-    const SKETCHPAD_WIDTH = 16;
-    const SKETCHPAD_HEIGHT = 16;
+    const NUM_OF_HORIZONTAL_BOXES = 16;
+    const NUM_OF_VERTICAL_BOXES = 16;
 
-    const sketchContainer = document.querySelector(".sketchContainer");
     let sketchBox = null;
+
+    const sketchContainerStyles = window.getComputedStyle(sketchContainer);
+    const sketchContainerWidth = parseInt(sketchContainerStyles.getPropertyValue('width'));
+    const sketchContainerHeight = parseInt(sketchContainerStyles.getPropertyValue('height'));
 
     /*
     TO GET INDEX OF CURRENT ELEMENT 
@@ -34,13 +39,58 @@ function generateSketchPad() {
     https://stackoverflow.com/questions/43660757/iteration-counter-for-double-loops
     */
 
-    for (let i = 0; i < SKETCHPAD_WIDTH; i++) {
-        for (let k = 0; k < SKETCHPAD_HEIGHT; k++) {
+    for (let i = 0; i < numOfBoxes; i++) {
+        for (let k = 0; k < numOfBoxes; k++) {
             sketchBox = document.createElement("div");
             sketchBox.classList.add("sketchBox");
-            sketchBox.setAttribute("id", k + SKETCHPAD_HEIGHT * i);
+            sketchBox.setAttribute("id", k + numOfBoxes * i);
+            sketchBox.setAttribute("style", `width: ${sketchContainerWidth / numOfBoxes}px; height: ${sketchContainerHeight / numOfBoxes}px;`)
             sketchContainer.appendChild(sketchBox);
         } 
     }
 }
 
+function deleteSketchPad() {
+    /*
+    INIT references to the entire list of sketch boxes
+    FOR each sketch box 
+        Delete node
+    END FOR
+    */
+
+    const sketchBoxes = document.querySelectorAll(".sketchContainer > .sketchBox");
+    for (const box of sketchBoxes) {
+        sketchContainer.removeChild(box);
+    }
+}
+
+function resizeSketchPad() {
+    /*
+    GET size of sketch pad from user
+    IF invalid num 
+
+    ELSE IF size is greater than 100 THEN
+        DISPLAY too large message
+    ELSE
+    
+    END IF
+
+    CALL deleteSketchPad to get rid of the already existing boxes
+    CALL generateSketchPad to create the new sketchpad
+    */
+    let numOfBoxes = (prompt("Enter the number of boxes: "));
+
+    console.log((+numOfBoxes), numOfBoxes);
+
+    if (numOfBoxes === "" || isNaN(+numOfBoxes)) {
+        alert("Please enter a valid value.");
+    } else if (numOfBoxes === null) {
+        // Do nothing
+    } else if (numOfBoxes > 100) {
+        alert("The max number of boxes is 100. Please enter a valid value.");
+    } else {
+        console.log("entered");
+        deleteSketchPad();
+        generateSketchPad(numOfBoxes);
+    }   
+}
